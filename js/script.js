6d5abc526ea6809ff672027594d08dda90e05ec8
Brody Maclean <hello@brody.com>
2017-03-27 20:37:27 +1100

    Updated social feature image

--- js/script.js
+++ js/script.js
@@ -49,7 +49,6 @@ $(function() {
   $(".video").click(function () {
     var theModal = $(this).data("target"),
     videoSRC = $(this).attr("data-video"),
-    // videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
     videoSRCauto = videoSRC + "?modestbranding=1&rel=0&html5=1&autoplay=1";
     $(theModal + ' iframe').attr('src', videoSRCauto);
     $(theModal + ' button.close').click(function () {
